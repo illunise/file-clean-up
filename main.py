db60c1808fcfd25637cc1d6bca28c03d6eda08aa
Manish <manishkus8421@gmail.com>
2023-03-09 13:04:42 +0530

    Created function to create folders if not exists

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import os
 import shutil
+
+def createFolder(folder):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+createFolder("images")
+createFolder("docs")
+createFolder("others")
+createFolder("videos")
