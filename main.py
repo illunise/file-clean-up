f135e7affc570ad6733f73b0a41a9d6e8534640b
Manish <manishkus8421@gmail.com>
2023-03-09 13:05:41 +0530

    Added Extensions list to iterate with

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ createFolder("images")
 createFolder("docs")
 createFolder("others")
 createFolder("videos")
+
+dir_list = os.listdir()
+
+image_ext = [".png", '.jpg', '.jpeg']
+docs_ext = [".docs", '.bat', '.bin', '.csv', 'doc', '.docx', '.html', '.htm', '.iso', '.pdf', '.ppt', '.psd', '.xls', '.xlsx']
+videos_ext = [".avi", '.gif', '.m4a', '.mov', '.mp4', '.mpeg']
